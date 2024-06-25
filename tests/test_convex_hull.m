8061b192fe4ef31efbac354d1c1e77014e48c35a
NicolasDouillet <nicolas.douillet@free.fr>
2024-06-25 23:00:28 +0200

    Put back test_convex_hull removed by mistake

---
+++ tests/test_convex_hull.m
@@ -0,0 +1,55 @@
+% test convex_hull
+
+clear all, close all, clc;
+
+addpath('../src');
+addpath('../data');
+
+
+nb_vtx = 128;
+X = 2*(rand(nb_vtx,1)-0.5);
+Y = 2*(rand(nb_vtx,1)-0.5);
+Z = 2*(rand(nb_vtx,1)-0.5);
+
+Rho = X.^2 + Y.^2 + Z.^2;
+i = Rho <= 1;
+X = X(i);
+Y = Y(i);
+Z = Z(i);
+V = cat(2,X,Y,Z);
+
+
+% filenames = {'sinusoidal_dodecahedron_MR';...
+%              'concave_Reuleaux_tetrahedron';...
+%              'torus_light';...
+%              };
+% 
+% id = 2; % 1, 2, 3, 4     
+% filename = strcat(cell2mat(filenames(id,1)),'.mat');         
+% load(filename);
+
+
+T_cv = convhull(V(:,1),V(:,2),V(:,3)); % Matlab (R) embeded function
+T_mcv = convex_hull(V);                % my Jarvis / gift wrapping algorithm
+[V_out,T_qcv] = quick_hull(V);         % my quickhull algorithm
+
+
+if isequal(sortrows(sort(T_mcv,2)),sortrows(sort(T_cv,2))) % no test with T_qcv since V_out and then triangulations are different
+   
+    disp('Same point sets and triangulations.');
+    
+else
+    
+    disp('Point sets and triangulations are different, possibly due to some coplanar points.');
+    
+end
+
+
+plot_mesh(V,T_cv);
+axis equal;
+
+plot_mesh(V,T_mcv);
+axis equal;
+
+plot_mesh(V_out,T_qcv);
+axis equal;
