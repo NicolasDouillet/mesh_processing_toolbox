a9fe3233d43bd4e747eeac0c5e43db49b58d727c
NicolasDouillet <nicolas.douillet@free.fr>
2024-07-10 15:09:48 +0200

    Update agin doc + live script

--- doc.m
+++ doc.m
@@ -9,16 +9,32 @@ load('kitten_components.mat');
 show_mesh_components(V,components);
 view(180,0);
 
-%% Example #2 : boundary and holes selection
-load('kitten_holed.mat');
-nmnfld_vtx_id = select_non_manifold_vertices(V,T,false);
-[V,T] = clone_solve_non_manifold_vertices(V,T,nmnfld_vtx_id);
-select_mesh_boundary_and_holes(V,T);
+%% Example #2 : boundary smoothing
+show_mesh_boundary_and_holes(V,T);
+shading interp;
+camlight left;
+alpha(0.5);
 view(180,0);
+axis off;
+
+boundary = detect_mesh_boundary_and_holes(T);
+nb_iterations = 2;
+ngb_degre = 6;
+V_out = smooth_mesh_boundary_and_holes(V,boundary,nb_iterations,ngb_degre);
+
+show_mesh_boundary_and_holes(V_out,T);
+shading interp;
+camlight left;
+alpha(0.5);
+view(180,0);
+axis off;
 
 %% Example #3 : hole filling
+load('kitten_holed.mat');
 [V,T] = remove_non_manifold_vertices(V,T);
 boundary = select_mesh_boundary_and_holes(V,T);
+shading flat;
+camlight left;
 view(0,0);
 
 max_perim_sz = 200;
@@ -50,36 +66,24 @@ shading flat;
 camlight left;
 view(-90,0);
 
-%% Example #6 : boundary smoothing
-load('kitten_components.mat');
-
-show_mesh_boundary_and_holes(V,T);
-shading interp;
+%% Exammple #6 : mesh oversampling
+load('sinusoidal_dodecahedron_MR.mat');
+plot_mesh(V,T);
+shading faceted;
 camlight left;
-alpha(0.5);
-view(180,0);
-axis off;
-
-boundary = detect_mesh_boundary_and_holes(T);
-
-nb_iterations = 2;
-ngb_degre = 6;
-V_out = smooth_mesh_boundary_and_holes(V,boundary,nb_iterations,ngb_degre);
-
-show_mesh_boundary_and_holes(V_out,T);
-shading interp;
+[V,T] = oversample_mesh(V,T); % 'default' / midedge or 'centre'
+plot_mesh(V,T);
+shading faceted;
 camlight left;
-alpha(0.5);
-view(180,0);
-axis off;
 
 %% Example #7 : mesh slicing
 load('Armadillo_20k.mat');
+
 thres = -11;
 n = [0 0 1];
 P = [0 0 thres];
-slices_nb_max_contours = 6;
 
+slices_nb_max_contours = 6;
 slc_step = 3;
 raw_edges_list = query_edges_list(T,'sorted');
 srt_itx_vtx_lsts = slice_mesh(V,T,n,P,true,slices_nb_max_contours,'vertical',slc_step);
