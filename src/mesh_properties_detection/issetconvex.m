1134b20e0b717d947be01d97229c7f252f9e1967
NicolasDouillet <nicolas.douillet@free.fr>
2024-10-26 14:31:31 +0200

    + test_issetconvex + improved issetconvex

--- src/mesh_properties_detection/issetconvex.m
+++ src/mesh_properties_detection/issetconvex.m
@@ -4,10 +4,15 @@ function isconvex = issetconvex(V)
 % convex hull. This means the set and its convex hull are
 % actually coincident. This also implies the set has no internal point
 % and is fully included and defined by its outer boundary / convex hull.
+% Note : four or more Coplanar points in the set should be avoided since
+% the convex hull algorithm doesn't always include them.
 %
 % Author : nicolas.douillet (at) free.fr, 2024.
 
 
+% Body
+precision = 1e4*eps;
+V = uniquetol(V,precision,'ByRows',true);
 T = convhull(V);
 vtx_list = unique(T(:));
 isconvex = isequal(vtx_list,(1:size(V,1))');
---
+++ tests/test_issetconvex.m
@@ -0,0 +1,33 @@
+% test issetconvex
+
+clc;
+
+addpath(genpath('../src'));
+addpath('../data');
+
+
+% Closed surfaces only
+filenames = {'tetrahedron';...                        % true  (1) expected 
+             'cube';...                               % true  (1) expected 
+             'octahedron';...                         % true  (1) expected 
+             'icosahedron';...                        % true  (1) expected                
+             'dodecahedron';...                       % true  (1) expected -> failure because of coplanar points
+             'dodecahedron2';...                      % true  (1) expected
+             'geoid_lvl_16';...                       % true  (1) expected
+             'ovoid';...                              % true  (1) expected
+             'Reuleaux_tetrahedron';                  % true  (1) expected
+             'sinusoidal_dodecahedron_LR';...         % false (0) expected
+             'sinusoidal_dodecahedron_MR';...         % false (0) expected
+             'sinusoidal_icosahedron_ULR';...         % false (0) expected
+             'sinusoidal_icosahedron_MR';...          % false (0) expected
+             'inverted_sinusoidal_icosahedron_MR';... % false (0) expected
+             'torus_light';...                        % false (0) expected
+             'kitten';...                             % false (0) expected 
+             'Gargoyle_5k';...                        % false (0) expected
+             'Armadillo_10k'};                        % false (0) expected
+
+filename = strcat(cell2mat(filenames(9,1)),'.mat');         
+load(filename);
+
+% plot_mesh(V,T);
+issetconvex(V)
